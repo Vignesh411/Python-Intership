90968141aaf027991b2ba1973d327a01f14058ec
Vignesh411 <85832174+Vignesh411@users.noreply.github.com>
2021-06-15 22:48:07 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,19 @@
+#Exercise 1
+a=100
+b=100
+c=100
+print("Exercise 1")
+print("A=",a/10)
+print("B=",b*50)
+print("C=",c+60)
+#Exercise 2
+Number="Eicht"
+Number=Number.replace('c','g')
+print("Exercise 2")
+print(Number)
+#Exercise 3
+a=4
+b=6.5
+print("Exercise 3")
+print(int(b))
+print(float(a))
